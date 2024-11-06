e7da20929648d9f443ecdc2319dddd49fe7443a8
obrunofelix <felixseixasbruno@gmail.com>
2024-11-06 15:30:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('')
+const email = "nicolasvianapagamentos@gmail.com";
+const senha = "jabiraca";
+localStorage.setItem(email, senha);
+
+const emailinput = document.getElementById("email");
+const senhainput = document.getElementById("senha");
+const formulario = document.getElementById("formulario_login");
+formulario.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+  const emaildigitado = emailinput.value;
+  const senhadigitada = senhainput.value;
+  
+  console.log(emaildigitado);
+  console.log(senhadigitada);
+  
+});
